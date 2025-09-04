fc852d28d911208c9854511cc3d8499d597d6849
Rishabh  Shukla <rishabh.shukla8@gmail.com>
2025-09-04 21:08:03 +0530

    Added LLD design patterns modules.
    Added LLD design patterns modules.

---
+++ LLD2/src/DesignPatterns/AdapterAndFacade/Client.java
@@ -0,0 +1,8 @@
+package DesignPatterns.AdapterAndFacade;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello from Adapter and Facade pattern example!");
+    }
+    
+}
